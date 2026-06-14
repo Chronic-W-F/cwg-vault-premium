6cafa53588ab14425f42a98e475fe47bb22416f7
Chronic-W-F <vandergriff4004@comcast.net>
2026-06-14 01:45:15 -0500

    Update typewriter.js

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,1 +1,18 @@
+function typeText(element, text, speed = 24) {
+  return new Promise((resolve) => {
+    let index = 0;
+    element.textContent = "";
 
+    function type() {
+      if (index < text.length) {
+        element.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, speed);
+      } else {
+        resolve();
+      }
+    }
+
+    type();
+  });
+}
